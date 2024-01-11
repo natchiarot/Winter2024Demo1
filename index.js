87086fd8328ba1e993c47e3f1999c7f0826598fd
jnchiarot <140640440+jnchiarot@users.noreply.github.com>
2024-01-11 10:02:20 -0330

    Added http common core example

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const port = 3010;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
+});
+server.listen(port, () => {
+  console.log(`Http server running at port ${port}`);
+});
